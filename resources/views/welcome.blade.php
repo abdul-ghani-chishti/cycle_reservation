ae731a40de3f5ba515cce92a530854bf6ad436ce
Abdul Ghani Chishti <abdulghanichishti195@gmail.com>
2024-09-05 15:13:55 +0200

    just a test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>LAPORSA | Pengaduan Masyarakat</title>
+  <title>LAPORSAA | Pengaduan Masyarakat</title>
 
   <link href="{{ asset('css/app.css') }}" rel="stylesheet">
   <link rel="icon" href="{{ asset('img/favicon.svg')}}">
